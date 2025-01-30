14ab2b507653af970f2ede7f99cddf317bbe3575
sogreatcomputerinstitute <destiny20202010@gmail.com>
2025-01-30 03:30:36 -0800

    Update server.js

--- server.js
+++ server.js
@@ -27,6 +27,14 @@ io.on('connection', (socket) => {
         }
     });
 
+    socket.on('mediaStream', (streamObj) => {
+        const username = users[socket.id];
+        userStreams[socket.id] = streamObj;
+        console.log('Media stream received.');
+        // Broadcast the stream to all connected clients
+        socket.broadcast.emit('mediaStream', streamObj, username);
+    });
+
     socket.on('disconnect', () => {
         const username = users[socket.id];
         console.log('A user disconnected:', socket.id);
@@ -35,14 +43,9 @@ io.on('connection', (socket) => {
         io.emit('userLeft', username);
         io.emit('updateUsers', users);
     });
+});
 
-  socket.on('mediaStream', (streamObj) => {
-        const username = socket.username;
-        console.log('Media stream received.');
-        // Broadcast the stream to all connected clients
-        socket.broadcast.emit('mediaStream', streamObj, username);
-    });
-
-server.listen(3000, () => {
-    console.log('Server is listening on port 3000');
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
 });
